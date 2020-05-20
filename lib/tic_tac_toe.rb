fb6512489094a55393143250501d6a2284bea32e
Jason Chilcott <jasonchilcott@me.com>
2020-05-20 23:02:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,6 +45,20 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if (space == "X" || space == "O")
+      turns += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
 def play(board)
   turns = 0
   while turns < 9
